909bd515594c28d40c132c12c307c9947816067a
GerVatto <vattovazg@gmail.com>
2023-11-01 11:15:35 -0300

    cambiamos CSS, algunas clases y acomodamos los botones y las cards

--- main.js
+++ main.js
@@ -11,14 +11,25 @@ function guardar() {
 }
 console.log(formulario) ;
 //esta funcion es para cuando quede organizado lo de las cartas//
-(function(){
-    let card = document.querySelectorAll('.card-content');
-    for(let i = 0; i < card.length; i++){
-      card[i].addEventListener('mouseleave', function(event) {
-        let target = event.target;
-        if(target.scrollTop) {
-          target.scrollTop = 0;
-        }
-      });
-    };
-  })();
+// (function(){
+//     let card = document.querySelectorAll('.card-content');
+//     for(let i = 0; i < card.length; i++){
+//       card[i].addEventListener('mouseleave', function(event) {
+//         let target = event.target;
+//         if(target.scrollTop) {
+//           target.scrollTop = 0;
+//         }
+//       });
+//     };
+//   })();
+
+
+    // Obtener el botón "Leer más" y la tarjeta
+    const botonLeerMas = document.querySelector('boton1');
+    const tarjeta = document.getElementById('card1');
+  
+    // Agregar un evento de clic al botón "Leer más"
+    botonLeerMas.addEventListener('click', function() {
+      // Agregar o quitar una clase para expandir o contraer la tarjeta
+      tarjeta.idList.toggle('card1');
+    });
