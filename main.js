42b1ec67c90028b73d118cb0a16f0e934511b6fa
GerVatto <vattovazg@gmail.com>
2023-11-04 11:01:12 -0300

    cambios segun correccion

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 function guardar() {
 
     let nombreyapellido = document.getElementById("nombreyapellidoIngresado").value ;
-    let email = document.getElementById("emailIngresado").value ;
-    let tumensaje = document.getElementById("tumensajeIngresado").value ;
+    let email = document.getElementById("emailingresado").value ;
+    let tumensaje = document.getElementById("tumensaje").value ;
 
-    document.getElementById("nombre y apellido").innerHTML = nombreyapellido ;
-    document.getElementById("e-mail").innerHTML = email ;
-    document.getElementById("Tu mensaje").innerHTML = tumensaje ;
+    document.getElementById("nombreyapellidoIngresado").innerHTML = nombreyapellido ;
+    document.getElementById("emailingresado").innerHTML = email ;
+    document.getElementById("tumensaje").innerHTML = tumensaje ;
 
 }
-console.log(formulario) ;
+// console.log(formulario);
+
+
 //esta funcion es para cuando quede organizado lo de las cartas//
 // (function(){
 //     let card = document.querySelectorAll('.card-content');
@@ -36,14 +38,35 @@ console.log(formulario) ;
 
 
 
-let mostrando = false;
-function mostrar() {
-    let div = document.getElementById('verMas');
-    if(mostrando){
+let mostrando1 = false;
+function mostrar1() {
+    let div = document.getElementById('verMas1');
+    if(mostrando1){
         div.style.display = "none";
     } else{
         div.style.display = "block";
     }
-    mostrando = !mostrando;
+    mostrando1 = !mostrando1;
 }
-  
+
+let mostrando2 = false;
+function mostrar2() {
+    let div = document.getElementById('verMas2');
+    if(mostrando2){
+        div.style.display = "none";
+    } else{
+        div.style.display = "block";
+    }
+    mostrando2 = !mostrando2;
+}
+
+let mostrando3 = false;
+function mostrar3() {
+    let div = document.getElementById('verMas3');
+    if(mostrando3){
+        div.style.display = "none";
+    } else{
+        div.style.display = "block";
+    }
+    mostrando3 = !mostrando3;
+}
