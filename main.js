8077dff56f5bd137033ab76dd98d5b1a6cffa599
GerVatto <vattovazg@gmail.com>
2023-11-08 19:48:42 -0300

    ultima correccion

--- main.js
+++ main.js
@@ -9,32 +9,7 @@ function guardar() {
     document.getElementById("tumensaje").innerHTML = tumensaje ;
 
 }
-// console.log(formulario);
 
-
-//esta funcion es para cuando quede organizado lo de las cartas//
-// (function(){
-//     let card = document.querySelectorAll('.card-content');
-//     for(let i = 0; i < card.length; i++){
-//       card[i].addEventListener('mouseleave', function(event) {
-//         let target = event.target;
-//         if(target.scrollTop) {
-//           target.scrollTop = 0;
-//         }
-//       });
-//     };
-//   })();
-
-
-   /* // Boton "Leer más" y la tarjeta
-    const botonLeerMas = document.querySelector('boton1');
-    const tarjeta = document.getElementById('card1');
-  
-    // Agregar un evento de clic al botón "Leer más"
-    botonLeerMas.addEventListener('click', function() {
-      // Agregar o quitar una clase para expandir o contraer la tarjeta
-      tarjeta.idList.toggle('card1');
-    });*/
 
 
 
@@ -66,7 +41,7 @@ function mostrar3() {
     if(mostrando3){
         div.style.display = "none";
     } else{
-        div.style.display = "block";
+        div.style.display = "block";   
     }
     mostrando3 = !mostrando3;
 }
