67c479692b9c527965bafa4004a2a2ebb17511ea
Noelia Canabal <104909246+NoeliaUXUI@users.noreply.github.com>
2023-11-09 00:28:50 -0300

    arreglo cards

--- main.js
+++ main.js
@@ -16,10 +16,15 @@ function guardar() {
 let mostrando1 = false;
 function mostrar1() {
     let div = document.getElementById('verMas1');
+    const card1 = document.getElementById('card1')
+    const card2 = document.getElementById('card2');
+    console.log(card2.clientHeight)
     if(mostrando1){
         div.style.display = "none";
+        card1.style.height = '650px';
     } else{
         div.style.display = "block";
+        card1.style.height = 650 + div.clientHeight + 'px'
     }
     mostrando1 = !mostrando1;
 }
@@ -27,10 +32,13 @@ function mostrar1() {
 let mostrando2 = false;
 function mostrar2() {
     let div = document.getElementById('verMas2');
+    const card2 = document.getElementById('card2');
     if(mostrando2){
         div.style.display = "none";
+        card2.style.height = '650px';
     } else{
         div.style.display = "block";
+        card2.style.height = 650 + div.clientHeight + 'px'
     }
     mostrando2 = !mostrando2;
 }
@@ -38,10 +46,13 @@ function mostrar2() {
 let mostrando3 = false;
 function mostrar3() {
     let div = document.getElementById('verMas3');
+    const card3 = document.getElementById('card3')
     if(mostrando3){
         div.style.display = "none";
+        card3.style.height = '650px';
     } else{
         div.style.display = "block";   
+        card3.style.height = 650 + div.clientHeight + 'px'
     }
     mostrando3 = !mostrando3;
 }
